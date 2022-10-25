c8043197b7ea1850666e9dd218e8bbdc7218742a
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2022-10-25 17:14:06 -0300

    Feat: created and configured HeaderBar component

---
+++ components/HeaderBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="bg-darkness">
+    <nav>
+      <div class="container py-3 px-2 d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center">
+          <figure class="mb-0 me-1">
+            <img class="img-fluid d-block" style="width:42px" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+          </figure>
+          
+          <ul class="mb-0">
+            <NuxtLink to="/" class="btn btn-sm btn-dark mb-0">Home</NuxtLink>
+            <NuxtLink to="/" class="btn btn-sm btn-dark mb-0">Categorias</NuxtLink>
+          </ul>
+        </div>
+
+        <div class="d-flex">  
+          <button type="button" style="width:44px;height:44px;" class="border-0 p-0 overflow-hidden rounded-circle" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+            <img class="img-fluid rounded-circle p-0" width="52px" height="52px" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </button>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBar'
+}
+</script>
+<style>
+  .bg-darkness{
+    background:#202020;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <HeaderBar />
     <Nuxt />
   </div>
 </template>
 
 <script>
+  import HeaderBar from '../components/HeaderBar.vue'
     export default {
         name: '',
+        components:{
+          HeaderBar
+        },
         data() {
             return {
             };
