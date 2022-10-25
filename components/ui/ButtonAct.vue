3cf024873ba49851133272a5127ca993fde78bb5
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2022-10-25 15:47:05 -0300

    Feat: adding ui components

---
+++ components/ui/ButtonAct.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="d-flex justify-content-center btn btn-primary" @click="$emit('click')">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonAct'
+}
+</script>
