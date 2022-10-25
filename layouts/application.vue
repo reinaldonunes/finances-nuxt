711d1082b27242534652a5260ec02cd4c735117b
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2022-10-25 15:47:57 -0300

    Feat: add default layout to application

---
+++ layouts/application.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {
+            };
+        },
+        methods: {},
+    };
+</script>
