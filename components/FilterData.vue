ed0edfa48fa3520f78013bb6f19f83648bb9672a
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2022-10-26 14:22:27 -0300

    Style & Feat: changed style from transaction list and added list items to transactions

---
+++ components/FilterData.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row mt-5 pb-5 border-bottom">
+    <div class="col-md-3 col-12 mb-2">
+      <AppFormLabel>Descrição</AppFormLabel>
+      <AppFormInput />
+    </div>
+
+    <div class="col-md-3 col-12 mb-2">
+      <AppFormLabel>Categoria</AppFormLabel>
+      <AppFormSelect :options="[{ name: 'Licença de softwares', id: 1 }]" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppButton from '@/components/ui/ButtonAct';
+import AppFormInput from '@/components/ui/InputForm';
+import AppFormLabel from '@/components/ui/LabelForm';
+import AppFormSelect from '@/components/ui/SelectForm';
+
+export default {
+  name: 'FilterData',
+  components: {
+    AppButton,
+    AppFormInput,
+    AppFormLabel,
+    AppFormSelect,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,22 @@
 <template>
   <div class="d-grid gap-3">
     <TransactionBar />
-    <div class="mt-5 w-75 pb-5 border-bottom d-flex align-items-center">
-      <div class="mr-2">
-        <AppFormLabel>Descrição</AppFormLabel>
-        <AppFormInput />
-      </div>
-
-      <div>
-        <AppFormLabel>Categoria</AppFormLabel>
-        <AppFormSelect :options="[{ name: 'Licença de softwares', id: 1 }]" />
-      </div>
-    </div>
+    <FilterData />
+    <TransactionsList />
   </div>
 </template>
 
 <script>
-import AppButton from '@/components/ui/ButtonAct';
-import AppFormInput from '@/components/ui/InputForm';
-import AppFormLabel from '@/components/ui/LabelForm';
-import AppFormSelect from '@/components/ui/SelectForm';
 import TransactionBar from '@/components/TransactionBar';
+import FilterData from '@/components/FilterData';
+import TransactionsList from "@/components/TransactionsList"
 
 export default {
   name: 'IndexPage',
   components: {
-    AppButton,
-    AppFormInput,
-    AppFormLabel,
-    AppFormSelect,
     TransactionBar,
+    FilterData,
+    TransactionsList
   },
   data() {
     return {
